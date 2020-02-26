535313930df7669ff6790f256a19b3ad46ae78ab
Sami Viitala <samiviitala89@gmail.com>
2020-02-26 13:57:15 +0200

    Refactor InputManager and ball

---
+++ Assets/Scripts/GolfBall.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class GolfBall : MonoBehaviour {
+
+    public Vector2 MinSpeed;
+    public Vector2 MaxSpeed;
+    private Rigidbody2D rb;
+    private LineRenderer lineRenderer;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ClampSpeed();
+    }
+
+    private bool CanShoot => rb.velocity.x < MinSpeed.x && rb.velocity.y < MinSpeed.y;
+
+    public void Shoot(float power, Vector2 direction)
+    {
+        if (!CanShoot)
+        {
+            Debug.Log("Cannot shoot, ball velocity above min speed");
+            return;
+        }
+        rb.velocity = direction * power;
+    }
+
+    private void ClampSpeed()
+    {
+        rb.velocity = new Vector2(
+             Math.Min(rb.velocity.x, MaxSpeed.x),
+             Math.Min(rb.velocity.y, MaxSpeed.y)
+        );
+    }
+
+    public void ShowLineRender(bool show)
+    {
+        lineRenderer.enabled = show;
+    }
+
+    public void UpdateLineRenderer(Vector3 startPoint, Vector3 endPoint)
+    {
+        startPoint.z = -1;
+        endPoint.z = -1;
+        lineRenderer.SetPosition(0, startPoint);
+        lineRenderer.SetPosition(1, endPoint);
+    }
+}
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,15 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    private ball ball;
+    private GolfBall ball;
+
+    private Vector3 mouseDownStartPosition;
+    private bool isMouseDown;
 
     // Start is called before the first frame update
     void Start()
     {
-        ball = FindObjectOfType<ball>();
+        ball = FindObjectOfType<GolfBall>();
     }
 
     // Update is called once per frame
@@ -17,13 +18,24 @@ public class InputManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            ball.isAiming = true;
-            ball.aimStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseDownStartPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            isMouseDown = true;
+            ball.ShowLineRender(true);
         }
 
         if (Input.GetMouseButtonUp(0))
         {
-            ball.isAiming = false;
+            isMouseDown = false;
+            var power = (mouseDownStartPosition - Camera.main.ScreenToWorldPoint(Input.mousePosition)).magnitude;
+            var direction = (mouseDownStartPosition - Camera.main.ScreenToWorldPoint(Input.mousePosition)).normalized;
+            ball.Shoot(power, direction);
+            ball.ShowLineRender(false);
         }
+
+        if (isMouseDown)
+        {
+            ball.UpdateLineRenderer(mouseDownStartPosition, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
+        
     }
 }
